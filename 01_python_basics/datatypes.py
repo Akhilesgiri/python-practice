53415822838d1a1c506732f72a15fbb83255b665
Akhil esgiri <akhilesgiri2440@gmail.com>
2026-05-17 22:49:25 +0530

    Completed data types practice
    Learned and practiced string, integer, float, and boolean data types using examples and type() function.

---
+++ 01_python_basics/datatypes.py
@@ -0,0 +1,58 @@
+
+# PYTHON DATA TYPES
+
+# A data type tells Python:
+# what kind of value is stored inside a variable.
+
+course = "Python"
+print(type(course))
+age = 22
+print(type(age))
+temperature = 36.5
+print(type(temperature))
+has_job = True
+print(type(has_job))
+
+
+
+# None means:
+# no value
+# empty value
+# nothing assigned
+
+profile_photo = None
+middle_name = None
+print(profile_photo)
+print(middle_name)
+print(type(profile_photo))
+print(type(middle_name))
+
+
+my_name ="ESGIRI AKHIL"
+age = 25
+weight = 66.4
+has_experience = True
+print(type(my_name))
+print(type(age))
+print(type(weight))
+print(type(has_experience))
+
+
+x = 10
+y = "10"
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+
+
+
+first_company_name = "USKCORP,INDIA"
+employee_count = 20
+rating = 4.8
+print("Company Name :", first_company_name)
+print("Employee Count :", employee_count)
+print("Company Rating :", rating)
+print(type(first_company_name))
+print(type(employee_count))
+print(type(rating))
