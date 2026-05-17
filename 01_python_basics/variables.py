8148fb1cf2a5f1aa5bb09fc4dcca2469d3274b02
Akhil esgiri <akhilesgiri2440@gmail.com>
2026-05-17 21:52:31 +0530

    Completed variables practice

---
+++ 01_python_basics/variables.py
@@ -0,0 +1,108 @@
+# What is a Variable?
+# A variable is used to store data in memory.
+# We can store:
+# - text
+# - numbers
+# - decimal values
+# - true/false values
+#
+# Think of a variable like a container
+# that stores some value.
+
+
+# String means text data.
+# Strings are written inside quotes.
+name = "Akhil"
+print(name)
+
+
+# Integer means whole numbers.
+age = 22
+print(age)
+
+
+# Float means decimal numbers.
+height = 5.8
+print(height)
+
+
+# Boolean stores only:
+# True or False
+is_python_developer = True
+print(is_python_developer)
+
+
+
+city = "Hyderabad"
+salary = 45000
+
+print(city)
+print(salary)
+
+
+print("City:", city)
+print("Salary:", salary)
+
+
+#type
+print(type(name))       # string
+print(type(age))        # integer
+print(type(height))     # float
+print(type(is_python_developer))  # boolean
+
+
+
+x = 10
+print(x)
+
+# Reassigning new value
+x = 20
+print(x)
+
+
+
+first_name = "Akhil"
+last_name = "Esgiri"
+
+print(first_name)
+print(last_name)
+
+
+# Uncommenting below lines will give errors
+
+# 1name = "Akhil"     # Cannot start with number
+# user-name = "Akhil" # Hyphen not allowed
+# class = "Python"    # Reserved keyword
+
+
+# Python treats uppercase and lowercase
+# variable names differently.
+
+name = "Akhil"
+Name = "Python"
+
+print(name)
+print(Name)
+
+
+user_name = "Akhil"
+user_age = 22
+is_logged_in = True
+
+print(user_name)
+print(user_age)
+print(is_logged_in)
+
+
+college_name = "Sri Vijaya Sai Junior College"
+favorite_subject = "English"
+college_experience = "Good"
+
+print("College Name :", college_name)
+print("favourite subject :",favorite_subject)
+print("college Experience :", college_experience)
+
+#types
+print(type(college_name))
+print(type(favorite_subject))
+print(type(college_experience))
