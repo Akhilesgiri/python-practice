d5ad04d77a5d5217dbad4c2362c6d1c4898ea395
Akhil esgiri <akhilesgiri2440@gmail.com>
2026-05-17 23:37:11 +0530

    completed learning type conversion
    I have learned about how we can change one type to another

---
+++ 01_python_basics/type_conversion.py
@@ -0,0 +1,61 @@
+
+# TYPE CONVERSION IN PYTHON
+
+# Type conversion means:
+# changing one data type into another.
+
+
+number = "10"
+converted_number = int(number)
+print(converted_number)
+print(type(converted_number))
+
+
+# =========================================
+# STRING TO FLOAT
+# =========================================
+
+price = "99.99"
+converted_price = float(price)
+print(converted_price)
+print(type(converted_price))
+
+
+# =========================================
+# INTEGER TO STRING
+# =========================================
+
+age = 22
+converted_age = str(age)
+print(converted_age)
+print(type(converted_age))
+
+
+# =========================================
+# FLOAT TO INTEGER
+# =========================================
+
+height = 5.8
+converted_height = int(height)
+print(converted_height)
+print(type(converted_height))
+
+
+# =========================================
+# INTEGER TO FLOAT
+# =========================================
+
+marks = 95
+converted_marks = float(marks)
+print(converted_marks)
+print(type(converted_marks))
+
+
+# =========================================
+# BOOLEAN TO STRING
+# =========================================
+
+has_job = True
+converted_has_job = str(has_job)
+print(converted_has_job)
+print(type(converted_has_job))
